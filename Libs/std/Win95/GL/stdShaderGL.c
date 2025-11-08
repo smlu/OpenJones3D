f085bfc1208767befe380c11beddebdde39bd1ba
Crt Vavros <4377556+smlu@users.noreply.github.com>
2025-11-08 20:29:26 +0100

    [std] Fix freeing null pointer

--- Libs/std/Win95/GL/stdShaderGL.c
+++ Libs/std/Win95/GL/stdShaderGL.c
@@ -307,8 +307,8 @@ GLShaderProgram* stdShader_CompileAndCreate(const char* pName, const char* pVert
     char* fsrc = stdShader_readGLSLFile(pPixelShaderCode);
     if (!vsrc || !fsrc)
     {
-        STDFREE(vsrc);
-        STDFREE(fsrc);
+        if ( vsrc ) STDFREE(vsrc);
+        if ( fsrc ) STDFREE(fsrc);
         return NULL;
     }
 
