7ac3289628f7f2145b5ea8414f63f51ad65cb3d9
HeilanMo <68738336+HeilanMo@users.noreply.github.com>
2026-03-16 17:40:02 +0100

    [std] added render flag for disable culling (OpenGL).

--- Libs/std/types.h
+++ Libs/std/types.h
@@ -254,7 +254,8 @@ typedef enum eStd3DRenderState
     STD3D_RS_ZWRITE_DISABLED       = 0x2000,
     STD3D_RS_FOG_ENABLED           = 0x8000,
     STD3D_RS_TEXFILTER_ANISOTROPIC = 0x10000, // Added
-    STD3D_BLEND_ENABLED            = 0x20000  // Added
+    STD3D_BLEND_ENABLED            = 0x20000, // Added
+    STD3D_CULL_DISABLED            = 0x40000, // Added
 } Std3DRenderState;
 
 #ifdef J3D_OPENGL
